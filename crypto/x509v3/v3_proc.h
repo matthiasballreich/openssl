fff637cc6cfa3abab239e2eb98522b523001c506
Matthias Ballreich <Matthias.Ballreich@outlook.de>
2018-10-15 17:57:12 +0200

    Update v3_proc.h

--- crypto/x509v3/v3_proc.h
+++ crypto/x509v3/v3_proc.h
@@ -1,19 +1,21 @@
 #ifndef HEADER_V3_PROCURATION_H
-# define HEADER_V3_PROCURATION_H
+#define HEADER_V3_PROCURATION_H
 
 struct ProcurationSyntax_st {
 	ASN1_PRINTABLESTRING* country,
-	ASN1_STRING* typeOfSubstition,
+	ASN1_STRING* typeOfSubstition, /* i.e. DIRECTORYSTRING */
 	SINGING_FOR* signingFor
-}
+};
 
 struct SigningFor_st {
 	GENERAL_NAME* thirdPerson,
 	ISSUER_SERIAL* certRef
-}
+};
 
 struct IssuerSerial_st {
 	GENERAL_NAMES* issuer,
 	ASN1_INTEGER* serial,
 	ASN1_BIT_STRING* issuerUID
-}
+};
+
+#endif
