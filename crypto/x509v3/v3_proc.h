6997ac265d962f07b108bf6003f031fb2a7c1dc7
Matthias Ballreich <Matthias.Ballreich@outlook.de>
2018-10-15 17:54:49 +0200

    Create v3_proc.h

---
+++ crypto/x509v3/v3_proc.h
@@ -0,0 +1,16 @@
+struct ProcurationSyntax_st {
+	ASN1_PRINTABLESTRING* country,
+	ASN1_STRING* typeOfSubstition,
+	SINGING_FOR* signingFor
+}
+
+struct SigningFor_st {
+	GENERAL_NAME* thirdPerson,
+	ISSUER_SERIAL* certRef
+}
+
+struct IssuerSerial_st {
+	GENERAL_NAMES* issuer,
+	ASN1_INTEGER* serial,
+	ASN1_BIT_STRING* issuerUID
+}
